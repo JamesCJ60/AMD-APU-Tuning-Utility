f39c721e991b9029214b8d1b0a43ee36918672b7
james <james@DESKTOP-5B75COV>
2021-08-10 23:33:51 +0100

    Add project files.

---
+++ Notification/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace Notification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.lblBody = new System.Windows.Forms.Label();
+            this.Close = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Notification.Properties.Resources.AATU_LOGO;
+            this.pictureBox1.Image = global::Notification.Properties.Resources.AATU_LOGO;
+            this.pictureBox1.Location = new System.Drawing.Point(7, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(76, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Location = new System.Drawing.Point(90, 9);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(64, 20);
+            this.lblHeader.TabIndex = 1;
+            this.lblHeader.Text = "Header";
+            // 
+            // lblBody
+            // 
+            this.lblBody.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBody.Location = new System.Drawing.Point(92, 29);
+            this.lblBody.Name = "lblBody";
+            this.lblBody.Size = new System.Drawing.Size(186, 51);
+            this.lblBody.TabIndex = 2;
+            this.lblBody.Text = "Body";
+            // 
+            // Close
+            // 
+            this.Close.Enabled = true;
+            this.Close.Interval = 2750;
+            this.Close.Tick += new System.EventHandler(this.Close_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 89);
+            this.Controls.Add(this.lblBody);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Label lblBody;
+        private System.Windows.Forms.Timer Close;
+    }
+}
+
---
+++ Notification/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notification
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // width of ellipse
+            int nHeightEllipse // height of ellipse
+        );
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 10, 10));
+        }
+
+        
+
+        Timer t1 = new Timer();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            const int margin = 10;
+            int x = Screen.PrimaryScreen.WorkingArea.Right - this.Width - margin;
+            int y = Screen.PrimaryScreen.WorkingArea.Bottom - this.Height - margin;
+
+            this.Location = new Point(x,y);
+            this.TopMost = true;
+            string[] args = Environment.GetCommandLineArgs();
+
+            lblHeader.Text = args[2].Replace("-", " ");
+            lblBody.Text = args[3].Replace("-", " ");
+
+            Opacity = 0;      //first the opacity is 0
+
+            t1.Interval = 6;  //we'll increase the opacity every 10ms
+            t1.Tick += new EventHandler(fadeIn);  //this calls the function that changes opacity 
+            t1.Start();
+        }
+
+        void fadeIn(object sender, EventArgs e)
+        {
+            if (Opacity >= 1)
+                t1.Stop();   //this stops the timer if the form is completely displayed
+            else
+                Opacity += 0.05;
+        }
+
+        void fadeOut(object sender, EventArgs e)
+        {
+            if (Opacity <= 0)     //check if opacity is 0
+            {
+                t1.Stop();    //if it is, we stop the timer
+                Close();   //and we try to close the form
+            }
+            else
+                Opacity -= 0.05;
+        }
+
+        private void Close_Tick(object sender, EventArgs e)
+        {
+            
+
+            t1.Tick += new EventHandler(fadeOut);  //this calls the fade out function
+            t1.Start();
+
+            if (Opacity == 0)  //if the form is completly transparent
+                this.Close();   //resume the event - the program can be closed
+        }
+    }
+}
